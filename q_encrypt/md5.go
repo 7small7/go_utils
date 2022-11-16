64526cf4b72dd5d4fc6ce80c7ab5e31666c93608
7small7 <1005349393@qq.com>
2022-11-17 00:59:59 +0800

    verify empty for md5

--- q_encrypt/md5.go
+++ q_encrypt/md5.go
@@ -3,12 +3,24 @@ package q_encrypt
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"strings"
 )
 
+func emptyStrVerify(encryptStr string) (string, error) {
+	if encryptStr == "" {
+		return "", errors.New("加密字符串不能为空")
+	}
+	return encryptStr, nil
+}
+
 // Md5Encrypt 获取小写32位加密字符串
 func Md5Encrypt(encryptStr string) (string, error) {
+	_, err := emptyStrVerify(encryptStr)
+	if err != nil {
+		return "", err
+	}
 	str := md5.Sum([]byte(encryptStr))
 	md5str := fmt.Sprintf("%x", str)
 	return md5str, nil
@@ -16,6 +28,10 @@ func Md5Encrypt(encryptStr string) (string, error) {
 
 // Md516Encrypt 获取小写16位加密字符串
 func Md516Encrypt(encryptStr string) (string, error) {
+	_, err := emptyStrVerify(encryptStr)
+	if err != nil {
+		return "", err
+	}
 	h := md5.New()
 	h.Write([]byte(encryptStr))
 	return hex.EncodeToString(h.Sum(nil))[0:16], nil
@@ -23,12 +39,20 @@ func Md516Encrypt(encryptStr string) (string, error) {
 
 // Md5EncryptUpper 获取大写32位加密字符串
 func Md5EncryptUpper(encryptStr string) (string, error) {
+	_, err := emptyStrVerify(encryptStr)
+	if err != nil {
+		return "", err
+	}
 	encrypt, err := Md5Encrypt(encryptStr)
 	return strings.ToUpper(encrypt), err
 }
 
 // Md516EncryptUpper 获取大写16位加密字符串
 func Md516EncryptUpper(encryptStr string) (string, error) {
+	_, err := emptyStrVerify(encryptStr)
+	if err != nil {
+		return "", err
+	}
 	encrypt, err := Md516Encrypt(encryptStr)
 	return strings.ToUpper(encrypt), err
 }
